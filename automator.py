54258e0bf1d8ef818bbcff54c683631a0908ab68
Yunus Emre Tom <78315933+yunusemretom@users.noreply.github.com>
2024-05-26 19:37:57 +0300

    Updated
    Buttons have been renewed and the code for adding images has arrived.

--- automator.py
+++ automator.py
@@ -7,11 +7,15 @@ from webdriver_manager.chrome import ChromeDriverManager
 from time import sleep
 from urllib.parse import quote
 import os
+import time 
 
 options = Options()
 options.add_experimental_option("excludeSwitches", ["enable-logging"])
 options.add_argument("--profile-directory=Default")
 options.add_argument("--user-data-dir=/var/tmp/chrome_user_data")
+
+
+image_path = "IMG_20220809_105333.jpg" #imagenizin yolunu kendi bilgisayarınıza göre değiştiriniz.
 
 os.system("")
 os.environ["WDM_LOG_LEVEL"] = "0"
@@ -74,18 +78,39 @@ for idx, number in enumerate(numbers):
 			if not sent:
 				driver.get(url)
 				try:
+					"""
+					eğer resminiz var ise alttaki yorum satırını kaldırın
+					"""
 
-					click_btn = WebDriverWait(driver, delay).until(EC.element_to_be_clickable((By.XPATH, "//*[@id='main']/footer/div[1]/div/span[2]/div/div[2]/div[2]/button")))
+					"""
+					attach_button = WebDriverWait(driver, 10).until(
+						EC.element_to_be_clickable((By.XPATH, '//*[@id="main"]/footer/div[1]/div/span[2]/div/div[1]/div[2]/div/div'))
+					)
+					attach_button.click()
+					time.sleep(0.5)
+					# Dosya yükleme butonunu bul
+					image_box = WebDriverWait(driver, 10).until(
+						EC.presence_of_element_located((By.XPATH, '//*[@id="main"]/footer/div[1]/div/span[2]/div/div[1]/div[2]/div/span/div/ul/div/div[2]/li/div/input'))
+					)
+
+					image_box.send_keys(os.path.abspath(image_path))
+					
+					time.sleep(2)
+					"""
+					click_btn = WebDriverWait(driver, delay).until(EC.element_to_be_clickable((By.XPATH, "//span[@data-icon='send']")))
+					
 				except Exception as e:
+					print(e)
 					print(style.RED + f"\nFailed to send message to: {number}, retry ({i+1}/3)")
 					print("Make sure your phone and computer is connected to the internet.")
 					print("If there is an alert, please dismiss it." + style.RESET)
 				else:
 					sleep(1)
 					click_btn.click()
+					time.sleep(15)
 					sent=True
 					sleep(3)
 					print(style.GREEN + 'Message sent to: ' + number + style.RESET)
 	except Exception as e:
 		print(style.RED + 'Failed to send message to ' + number + str(e) + style.RESET)
-driver.close()
+#driver.close()

71ea6b7b54c8c2645a66bab84eee601717d100d8
Yunus Emre Tom <78315933+yunusemretom@users.noreply.github.com>
2024-06-27 20:57:25 +0300

    Add files via upload

--- automator.py
+++ automator.py
@@ -3,19 +3,22 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
-from webdriver_manager.chrome import ChromeDriverManager
 from time import sleep
+import time
 from urllib.parse import quote
-import os
-import time 
+import os 
 
 options = Options()
 options.add_experimental_option("excludeSwitches", ["enable-logging"])
 options.add_argument("--profile-directory=Default")
 options.add_argument("--user-data-dir=/var/tmp/chrome_user_data")
+options.add_argument('--start-maximized')
+options.add_argument("--disable-background-timer-throttling")  # Arka planda zamanlayıcı kısıtlamalarını devre dışı bırak
+options.add_argument("--disable-renderer-backgrounding")  # Arka planda render işlemlerini durdurmayı devre dışı bırak
 
 
-image_path = "IMG_20220809_105333.jpg" #imagenizin yolunu kendi bilgisayarınıza göre değiştiriniz.
+image_path = "IMG_20220624_115931.jpg" #imagenizin yolunu kendi bilgisayarınıza göre değiştiriniz.
+
 
 os.system("")
 os.environ["WDM_LOG_LEVEL"] = "0"
@@ -31,26 +34,19 @@ class style():
     UNDERLINE = '\033[4m'
     RESET = '\033[0m'
 
-print(style.BLUE)
-print("**********************************************************")
-print("**********************************************************")
-print("*****                                               ******")
-print("*****  THANK YOU FOR USING WHATSAPP BULK MESSENGER  ******")
-print("*****      This tool was built by Anirudh Bagri     ******")
-print("*****           www.github.com/anirudhbagri         ******")
-print("*****                                               ******")
-print("**********************************************************")
-print("**********************************************************")
-print(style.RESET)
 
+# ------- Mesajınızın bulunduğu dosya ------------#
 f = open("message.txt", "r", encoding="utf8")
 message = f.read()
 f.close()
 
+# ----- Burada mesajınızı gösterir --------#
 print(style.YELLOW + '\nThis is your message-')
 print(style.GREEN + message)
 print("\n" + style.RESET)
 message = quote(message)
+
+# --------- Burada göndermek istediğiniz numaları dosyadan çeker ---------- # 
 
 numbers = []
 f = open("numbers.txt", "r")
@@ -58,45 +54,66 @@ for line in f.read().splitlines():
 	if line.strip() != "":
 		numbers.append(line.strip())
 f.close()
+
+# ----------- Burada Numara sayısını yani mesaj sayısını gösterir ----------- #
+
 total_number=len(numbers)
 print(style.RED + 'We found ' + str(total_number) + ' numbers in the file' + style.RESET)
-delay = 30
 
-driver = webdriver.Chrome( options=options)
-print('Once your browser opens up sign in to web whatsapp')
-driver.get('https://web.whatsapp.com')
-input(style.MAGENTA + "AFTER logging into Whatsapp Web is complete and your chats are visible, press ENTER..." + style.RESET)
+
+delay = 30 # Elemenlar bulmak için gereken süre 
+
+# ----- Burada tarayıcıyı açar ------- #
+svc = webdriver.ChromeService(executable_path="chromedriver.exe")
+driver = webdriver.Chrome(service=svc,options=options)
+
+print("Tarayıcınız açıldığında web whatsapp'ta oturum açın (bir kere yapmanız yeterli olacaktır.)")
+
+driver.get('https://web.whatsapp.com') # web whatsapp sitesine gidilir. 
+
+# -------- Burada whatsapp'a girdiğinizi onaylaylarsınız ------ #
+input(style.MAGENTA + "Whatsapp Web'e giriş tamamlandıktan ve sohbetleriniz görünür hale geldikten sonra ENTER tuşuna basın..." + style.RESET)
+
+
+# ---------------- Burada gönderme işlemleri yapılıyor ---------- #
+
 for idx, number in enumerate(numbers):
 	number = number.strip()
 	if number == "":
 		continue
 	print(style.YELLOW + '{}/{} => Sending message to {}.'.format((idx+1), total_number, number) + style.RESET)
 	try:
+		
+
 		url = 'https://web.whatsapp.com/send?phone=' + number + '&text=' + message
 		sent = False
+
 		for i in range(3):
 			if not sent:
 				driver.get(url)
 				try:
-					"""
-					eğer resminiz var ise alttaki yorum satırını kaldırın
-					"""
-
-					"""
-					attach_button = WebDriverWait(driver, 10).until(
+					alert = driver.switch_to.alert  # Uyarıya geçiş yapın
+					print(alert.text)  # Uyarı metnini (isteğe bağlı olarak) yazdırın
+					alert.accept()  # Uyarıyı onaylayın
+					print("Uyarı onaylandı.")
+				except:
+					print("Uyarı bulunamadı.")
+				try:
+					
+					attach_button = WebDriverWait(driver, delay).until(
 						EC.element_to_be_clickable((By.XPATH, '//*[@id="main"]/footer/div[1]/div/span[2]/div/div[1]/div[2]/div/div'))
 					)
 					attach_button.click()
 					time.sleep(0.5)
 					# Dosya yükleme butonunu bul
-					image_box = WebDriverWait(driver, 10).until(
+					image_box = WebDriverWait(driver, delay).until(
 						EC.presence_of_element_located((By.XPATH, '//*[@id="main"]/footer/div[1]/div/span[2]/div/div[1]/div[2]/div/span/div/ul/div/div[2]/li/div/input'))
 					)
 
 					image_box.send_keys(os.path.abspath(image_path))
 					
 					time.sleep(2)
-					"""
+					
 					click_btn = WebDriverWait(driver, delay).until(EC.element_to_be_clickable((By.XPATH, "//span[@data-icon='send']")))
 					
 				except Exception as e:
@@ -107,10 +124,14 @@ for idx, number in enumerate(numbers):
 				else:
 					sleep(1)
 					click_btn.click()
-					time.sleep(15)
+					time.sleep(2)
 					sent=True
-					sleep(3)
+					sleep(2)
 					print(style.GREEN + 'Message sent to: ' + number + style.RESET)
+
 	except Exception as e:
 		print(style.RED + 'Failed to send message to ' + number + str(e) + style.RESET)
-#driver.close()
+
+# ------------ İşlemler bitince tarayıcı kapatılıyor --------------- #
+
+driver.close()

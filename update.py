e0799e6e0a4cfcff00fcca14c02f4d9c1f5abd8e
Yunus Emre Tom <yunusemretom@gmail.com>
2025-05-26 14:56:52 +0300

    Güncelleme: Uzak sürüm kontrolü için URL güncellendi ve sürüm numarası formatı düzeltildi

--- update.py
+++ update.py
@@ -2,9 +2,9 @@ import requests
 
 def check_version():
     current_version = "1.2.2"
-    url = "https://raw.githubusercontent.com/kullaniciadi/projeadi/main/version.txt"
+    url = "https://raw.githubusercontent.com/yunusemretom/whatsapp-bulk-messenger/refs/heads/primary/version.txt"
     remote_version = requests.get(url).text.strip()
-
+    print(f"Yerel sürüm: {current_version}, Uzak sürüm: {remote_version}")
     if current_version != remote_version:
         print("Yeni güncelleme var!")
         return True
@@ -21,9 +21,11 @@ def download_latest():
     print("Yeni sürüm indirildi.")
 
 
-
+check_version()
+"""
 if __name__ == "__main__":
     if check_version():
         download_latest()
     else:
         print("Zaten en son sürümdesiniz.")
+        """

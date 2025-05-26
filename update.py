94216e0e576dcfde83ae58f01ac918238248c98e
Yunus Emre Tom <yunusemretom@gmail.com>
2025-05-26 15:11:54 +0300

    Güncelleme: Sürüm kontrolü fonksiyonu güncellendi, URL düzeltildi ve ana dosya adı değiştirildi

--- update.py
+++ update.py
@@ -1,31 +1,46 @@
 import requests
 
-def check_version():
-    current_version = "1.2.2"
-    url = "https://raw.githubusercontent.com/yunusemretom/whatsapp-bulk-messenger/refs/heads/primary/version.txt"
+def check_version(current_version):
+    url = "https://raw.githubusercontent.com/yunusemretom/whatsapp-bulk-messenger/primary/version.txt"
     remote_version = requests.get(url).text.strip()
     print(f"Yerel sürüm: {current_version}, Uzak sürüm: {remote_version}")
-    if current_version != remote_version:
+
+    if current_version <= remote_version:
         print("Yeni güncelleme var!")
+        with open("version.txt", "w", encoding="utf-8") as f:
+            f.write(remote_version)
         return True
+    
     else:
         print("Güncel sürüm.")
         return False
-    
-def download_latest():
-    download_url = "https://github.com/yunusemretom/whatsapp-bulk-messenger/main.py"
-    r = requests.get(download_url)
 
-    with open("main_updated.py", "w", encoding="utf-8") as f:
+def get_local_version(file_path="version.txt"):
+    try:
+        with open(file_path, "r", encoding="utf-8") as file:
+            version = file.read().strip()
+            return version
+    except FileNotFoundError:
+        print("Yerel sürüm dosyası bulunamadı!")
+        return None
+    except Exception as e:
+        print(f"Hata oluştu: {e}")
+        return None
+
+def download_latest():
+    download_url = "https://raw.githubusercontent.com/yunusemretom/whatsapp-bulk-messenger/primary/main.py"
+    
+    r = requests.get(download_url)
+    with open("main.py", "w", encoding="utf-8") as f:
         f.write(r.text)
     print("Yeni sürüm indirildi.")
 
 
-check_version()
-"""
+
 if __name__ == "__main__":
-    if check_version():
+    version = get_local_version()
+    if version and check_version(version):
+        print("Güncelleme mevcut, indiriliyor...")
         download_latest()
     else:
-        print("Zaten en son sürümdesiniz.")
-        """
+        print("Güncelleme gerekli değil.")
